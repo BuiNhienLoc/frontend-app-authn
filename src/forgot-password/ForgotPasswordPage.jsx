82b15fd39a4188cf3861c86038d631a894fd3116
BuiNhienLoc <15635@student.vgu.edu.vn>
2023-08-15 08:56:12 +0000

    hehehe

--- src/forgot-password/ForgotPasswordPage.jsx
+++ src/forgot-password/ForgotPasswordPage.jsx
@@ -157,7 +157,7 @@ const ForgotPasswordPage = (props) => {
             <p className="mt-5.5 small text-gray-700">
               {formatMessage(messages['additional.help.text'], { platformName })}
               <span>
-                <Hyperlink isInline destination={`mailto:${getConfig().INFO_EMAIL}`}>{getConfig().INFO_EMAIL}</Hyperlink>
+                <Hyperlink isInline destination={`mailto:${getConfig().CONTACT_EMAIL}`}>{getConfig().CONTACT_EMAIL}</Hyperlink>
               </span>
             </p>
           </Form>
